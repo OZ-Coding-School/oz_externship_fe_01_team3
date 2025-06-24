974ab444f60a151288aa16a652f20a00fcfc20a1
이승준 <tmdwns092727@nate.com>
2025-06-24 14:52:41 +0900

    directory: 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router'
 import './App.css'
 import { Header } from '@/components/common/Header'
+import { router } from './pages/routes'
 
 function App() {
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        <Header />
-      </div>
+      template
+      <RouterProvider router={router} />
+      <Header />
     </QueryClientProvider>
   )
 }
