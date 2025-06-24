d49537b003cc0112a3453fd8752ce6c8f78e43d6
허성진 <heoseongjin@heoseongjin-ui-MacBookPro-2.local>
2025-06-24 14:27:19 +0900

    리베이스 충돌 해결 #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import { RouterProvider } from 'react-router'
 import './App.css'
 import { Header } from '@/components/common/Header'
-import RadioType from '@/components/examQuestions/RadioType'
-import CheckBoxType from './components/examQuestions/CheckBoxType'
 import { router } from './pages/routes'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ExampleListContainer from './components/examList/ExampleContainer'
+import RadioType from './components/examQuestions/RadioType'
+import CheckBoxType from './components/examQuestions/CheckBoxType'
+import ReorderQuestion from './components/examQuestions/ReorderQuestion'
 
 function App() {
   const queryClient = new QueryClient()
-=======
-import RadioType from './components/examQuestions/RadioType'
-=======
->>>>>>> f251cb9 (리베이스 충돌 해결 #2)
-
 function App() {
   const test = [
     { id: 1, test: '나는야 승준티비 대빵이지' },
     { id: 2, test: '나는야 지향티비 열정걸이지' },
     { id: 3, test: '나는야 우수티비 매우 우수하지' },
     { id: 4, test: '나는야 성진티비 입대하지' },
-  ]
->>>>>>> 5395a56 (리베이스 충돌 해결)
 
   const test2 = [
     { id: 1, OX: 'O', test: '맞아요' },
@@ -32,22 +24,17 @@ function App() {
   return (
     <div>
       template
-<<<<<<< HEAD
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <Header />
         <ExampleListContainer />
       </QueryClientProvider>
-=======
       <RouterProvider router={router} />
       <Header />
       <br />
       <br />
       <br />
       <RadioType options={test} questionId="w"></RadioType>
-<<<<<<< HEAD
->>>>>>> 5395a56 (리베이스 충돌 해결)
-=======
       <br />
       <br />
       <br />
@@ -56,7 +43,6 @@ function App() {
       <br />
       <br />
       <ReorderQuestion options={test} questionId="s"></ReorderQuestion>
->>>>>>> f251cb9 (리베이스 충돌 해결 #2)
     </div>
   )
 }
