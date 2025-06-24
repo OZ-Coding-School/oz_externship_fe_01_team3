d426dd6f5bc41218df61cdd1dfd58d50bf89d1e1
이승준 <tmdwns092727@nate.com>
2025-06-24 14:56:08 +0900

    Provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
 import './App.css'
-import { Header } from '@/components/common/Header'
 import { router } from './pages/routes'
+import { Header } from '@/components/common/Header'
 
 function App() {
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
-      template
       <RouterProvider router={router} />
       <Header />
     </QueryClientProvider>
