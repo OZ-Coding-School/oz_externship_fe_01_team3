11222386f529e730da8740ee5448ad8a322d065d
han-nun <spshspshek@gmail.com>
2025-06-24 16:38:46 +0900

    ✨ feat: Toast 부분 Success, Error 분기 처리 및 디자인 2종 Props로 제어

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,20 +1,45 @@
-interface ToastProps {
-  toast: {
-    message: string
-    type: 'success' | 'error'
-  } | null
-}
+import type { ToastProps } from '@/types/common/Toast'
+import checkIcon from '@/assets/check.svg'
+import errorIcon from '@/assets/alert.svg'
 
-export const Toast = ({ toast }: ToastProps) => {
+export const Toast = ({ toast }: { toast: ToastProps | null }) => {
   if (!toast) return null
 
-  const bgColor = toast.type === 'success' ? 'bg-green-500' : 'bg-red-500'
+  const isError = toast.type === 'error'
+  const icon = isError ? errorIcon : checkIcon
+  const centeredTitleColor = isError ? 'text-[#ec0037]' : 'text-[#121212]'
+  const defaultTextColor = isError ? 'text-[#ec0037]' : 'text-[#4d4d4d]'
+
+  if (toast.layout === 'centered') {
+    return (
+      <div className="fixed flex flex-col items-center gap-2 justify-center w-[396px] h-[128px] top-[422px] left-[762px] bg-[#ffffff] rounded-[12px] text-center ">
+        <div className="w-[24px] h-[24px]">
+          <img src={icon} alt={toast.type} className="w-full h-full" />
+        </div>
+        <div
+          className={`text-[20px] ${centeredTitleColor} font-bold leading-[1.4] tracking-[-0.03em]`}
+        >
+          {toast.message}
+        </div>
+        {toast.subMessage && (
+          <div className="text-sm text-[#4d4d4d] leading-[1.4] tracking-[-0.03em]">
+            {toast.subMessage}
+          </div>
+        )}
+      </div>
+    )
+  }
 
   return (
     <div
-      className={`fixed left-1/2 bottom-1/2 -translate-x-1/2 px-4 py-2 rounded text-white ${bgColor}`}
+      className={`fixed top-[204px] left-[836px] flex items-center w-[248px] h-[48px] pl-1 px-3 bg-[#fafafa] border border-[#ECECEC] rounded-md [box-shadow:4px_4px_4px_0_rgba(131,131,131,0.25)] ${toast.className || ''}`}
     >
-      {toast.message}
+      <img src={icon} alt={toast.type} className="w-[24px] h-[24px] mr-1" />
+      <span
+        className={`text-[14px] font-medium leading-[1.4] tracking-[-0.03em] ${defaultTextColor} whitespace-nowrap`}
+      >
+        {toast.message}
+      </span>
     </div>
   )
 }
