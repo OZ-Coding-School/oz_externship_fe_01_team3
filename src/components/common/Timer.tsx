0a86486658d5866e7a483052cb748c47bd7ffc28
han-nun <spshspshek@gmail.com>
2025-06-25 15:29:51 +0900

    feat: rebase 오류 원인 해결

---
+++ src/components/common/Timer.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+
+interface TimerProps {
+  time?: number
+}
+
+const Timer = ({ time }: TimerProps) => {
+  const Minutes_In_Ms = time ? time * 60 * 1000 : 0
+  const Interval = 1000
+  const [timeLeft, setTimeLeft] = useState<number>(Minutes_In_Ms)
+
+  const minutes = String(Math.floor((timeLeft / (1000 * 60)) % 60)).padStart(
+    2,
+    '0'
+  )
+  const seconds = String(Math.floor((timeLeft / 1000) % 60)).padStart(2, '0')
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - Interval)
+    }, Interval)
+    if (timeLeft <= 0) {
+      clearInterval(timer)
+    }
+    return () => clearInterval(timer)
+  }, [timeLeft])
+
+  return (
+    <div>
+      {minutes}:{seconds}
+    </div>
+  )
+}
+
+export default Timer
