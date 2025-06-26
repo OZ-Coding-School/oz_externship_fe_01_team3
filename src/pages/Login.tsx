f9dfb7c9def28218287ff574196e4f5d50ce1946
creamymood <wbwbwlgid@naver.com>
2025-06-26 17:49:47 +0900

    Feat : Login 페이지 파일 생성

---
+++ src/pages/Login.tsx
@@ -0,0 +1,62 @@
+import SocialButton from "@/components/common/SocialButton";
+import { useForm } from "react-hook-form";
+
+
+export default function Login() {
+  const { register, handleSubmit, formState: { errors }, watch, setValue } = useForm();
+
+    return(
+            <div className="flex flex-col items-center pt-20">
+                <img src="src/assets/logo_black.png" alt="오즈코딩스쿨 로고" className="w-[180px] mb-[27px]"/>
+                <div className="flex gap-3 text-[16px]">
+                    <p className="text-[#4D4D4D]"> 회원 이신가요?</p>
+                    <p className="text-[#6201E0]"> 로그인 하기</p>
+                </div>
+                <br/>
+                <br/>
+    
+                {/* 카카오로 회원가입 버튼  */}
+                <SocialButton
+                    bgColor="#FEE500"
+                    txtColor="#391C1A"
+                    iconSrc="src/assets/kakao_brown.svg"
+                    iconAlt="카카오"
+                    marginBottom="16px"
+                    onClick={() => alert("카카오 로그인 클릭됨")}>
+                        카카오 간편 로그인 / 가입
+                </SocialButton>
+    
+                {/* 네이버로 회원가입 버튼  */}
+                <SocialButton
+                    bgColor="#03C75A"
+                    txtColor="#FFFFFF"
+                    iconSrc="src/assets/naver_white.svg"
+                    iconAlt="네이버"
+                    marginBottom="25px"
+                    onClick={() => alert("네이버 로그인 클릭됨")}>
+                        네이버 간편 로그인 / 가입
+                </SocialButton>
+
+                <input
+              {...register("email", {
+                required: "이메일을 입력해주세요.",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "유효한 이메일 주소를 입력해주세요.",
+                },
+              })}
+              className={`
+                w-[356px] h-[48px] rounded-[4px] pt-[10px] pr-[16px] pb-[10px] pl-[16px] mb-[16px]
+                placeholder-[#BDBDBD] text-[#333] border-[1px]
+                focus:outline-none focus:border-[#6201E0]
+                border-[#BDBDBD]
+              `}
+              placeholder="이메일을 입력해주세요"
+            />
+    
+                <p className="text-[#4D4D4D] underline"> 일반회원 가입</p>
+    
+            </div>
+    
+        )
+}
