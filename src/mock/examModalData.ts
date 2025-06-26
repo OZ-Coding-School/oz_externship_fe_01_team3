232c4923eca4e28275c23a58ab0bd5451fbfd7fe
이승준 <tmdwns092727@nate.com>
2025-06-26 14:49:05 +0900

    feat: 쪽지시험 목록 목데이터 Api 연동
    - 목데이터 추가
    - 모달 참가 코드 입력 시, POST 요청 후 데이터를 받아서 navigate로 보내주기

---
+++ src/mock/examModalData.ts
@@ -0,0 +1,25 @@
+import type { ModalSuccessResponse } from '@/types/examList/examModal'
+
+export const mockModalSuccessResponse: ModalSuccessResponse = {
+  test_id: 4,
+  title: '프론트엔드 기초 쪽지시험',
+  thumbnail_img_url: 'https://...',
+  elapsed_time: 30,
+  cheating_count: 0,
+  questions: [
+    {
+      question_id: 1,
+      type: '객관식',
+      question: 'HTML의 기본 구조를 이루는 태그는?',
+      options: ['<html>', '<head>', '<body>', '<div>'],
+      point: 5,
+    },
+    {
+      question_id: 2,
+      type: 'O/X',
+      question: 'CSS는 프로그래밍 언어이다.',
+      options: ['O', 'X'],
+      point: 5,
+    },
+  ],
+}
