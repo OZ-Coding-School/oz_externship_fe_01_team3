d94d423c10f07d0313da4ec118ef914d366de80d
허성진 <heoseongjin@heoseongjin-ui-MacBookPro-2.local>
2025-06-26 11:29:26 +0900

    Refactor: type => attributes 변경

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Header } from '@/components/common/Header'
 import { router } from './pages/routes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ExampleListContainer from './components/examList/ExampleContainer'
+import Button from './components/common/Button'
 
 function App() {
   const queryClient = new QueryClient()
@@ -14,6 +15,13 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <Header />
+        <Button
+          className="bg-amber-200 w-[40px] h-[20px]"
+          disabled={false}
+          type="submit"
+        >
+          sdas
+        </Button>
         <ExampleListContainer />
       </QueryClientProvider>
     </div>
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,32 +1,16 @@
-import type { ReactNode } from 'react'
+import type { ButtonHTMLAttributes, ReactNode } from 'react'
 
-export type ButtonStatus = 'button' | 'reset' | 'submit'
-
-interface ButtonProps {
-  disabled?: boolean
-  onClick?: () => void
-  type: ButtonStatus
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className: string
-  text?: string
   children?: ReactNode
 }
 
-export default function Button({
-  disabled = false,
-  onClick,
-  type = 'button',
-  className,
-  text,
-  children,
-}: ButtonProps) {
+export default function Button({ className, children, ...rest }: ButtonProps) {
   return (
     <button
       className={`${className} rounded-[4px] eading-[1.4] tracking-[-0.03] cursor-pointer`}
-      disabled={disabled}
-      onClick={onClick}
-      type={type}
+      {...rest}
     >
-      {text}
       {children}
     </button>
   )
