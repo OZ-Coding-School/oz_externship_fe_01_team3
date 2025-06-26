0572e6ba24db7417d3b51cceaa1770c54aade2d1
이승준 <tmdwns092727@nate.com>
2025-06-26 11:57:14 +0900

    feat: 시험 참가 모달 컴포넌트 UI 구현
    - 모달 컴포넌트 추가
    - 모달 컴포넌트 모달 닫기 버튼 추가 및 닫힘 토글링
    
    feat: #30

---
+++ src/components/examListModal/Modal.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+
+interface ModalProps {
+  setIsModalClose: (isModalOpen: boolean) => void
+}
+
+export default function Modal({ setIsModalClose }: ModalProps) {
+  const [examCode, setExamCode] = useState('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Regex:숫자만 허용하고 6자리까지만 제한
+    const regex = /^[0-9]*$/
+    if (regex.test(e.target.value) && e.target.value.length <= 6) {
+      setExamCode(e.target.value)
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (examCode.length !== 6) {
+      alert('6자리 숫자를 입력해주세요')
+      return
+    }
+
+    // TODO: 시험 시작 로직
+    console.log('시험 시작:', examCode)
+  }
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 ">
+      {/* 어두운 배경 */}
+      <div className="absolute inset-0 bg-[#121212] opacity-50" />
+      {/* 모달 */}
+      <form
+        onSubmit={handleSubmit}
+        className="relative z-10 w-[396px] h-[360px] bg-white rounded-[12px] flex flex-col items-center justify-center shadow-lg p-6"
+      >
+        <button
+          type="button"
+          className="absolute top-6 right-6 text-2xl text-[#9D9D9D]"
+          onClick={() => setIsModalClose(true)}
+        >
+          x
+        </button>
+        {/* 시험 아이콘 */}
+        <img
+          src="/src/assets/examList/python.png"
+          alt="python"
+          className="h-8 mb-4"
+        />
+        {/* 시험명 */}
+        <h2 className="text-lg font-semibold text-[#121212] my-4">
+          Python 심화
+        </h2>
+        {/* 문항수, 제한시간 */}
+        <p className="text-sm text-[#303030] mb-9 text-center">
+          총 10문항 · <span className="text-[#6201E0]">제한시간 20분</span>
+        </p>
+        {/* 참가 코드 입력 */}
+        <label
+          htmlFor="examCode"
+          className="self-start w-full text-[#222] font-medium"
+        >
+          참가 코드입력
+        </label>
+        <input
+          id="examCode"
+          type="password"
+          value={examCode}
+          onChange={handleChange}
+          placeholder="6자리를 입력해주세요"
+          maxLength={6}
+          className="w-full h-12 border border-[#E0E0E0] rounded-lg px-4 mb-6 text-lg focus:outline-[#6C47FF]"
+        />
+        {/* 시험시작 버튼 */}
+        <button className="w-full h-[48px] bg-[#6201E0] text-white text-[16px] font-semibold rounded-[4px]">
+          시험시작
+        </button>
+      </form>
+    </div>
+  )
+}
