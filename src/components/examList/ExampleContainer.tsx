91cfed90ce7e94eaa47115fd22e1cab5dfda3747
이승준 <tmdwns092727@nate.com>
2025-06-26 11:58:12 +0900

    feat: ExampleContainer 모달 플래그 추가
    - ExampleContainer에 모달 플래그 추가
    - Modal 컴포넌트에 모달 닫기 버튼 추가 및 닫기

--- src/components/examList/ExampleContainer.tsx
+++ src/components/examList/ExampleContainer.tsx
@@ -1,7 +1,11 @@
-import ExamList from './ExamList'
-import SideBar from './SideBar'
+import { useState } from 'react'
+import Modal from '@/components/examListModal/Modal'
+import ExamList from '@/components/examList/ExamList'
+import SideBar from '@/components/examList/SideBar'
 
 export default function ExampleListContainer() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   return (
     <div className="min-h-screen mt-[108px]">
       <div className="container mx-auto max-w-7xl min-h-screen  px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-24">
@@ -19,6 +23,8 @@ export default function ExampleListContainer() {
           </div>
         </div>
       </div>
+
+      {!isModalOpen && <Modal setIsModalClose={setIsModalOpen} />}
     </div>
   )
 }
