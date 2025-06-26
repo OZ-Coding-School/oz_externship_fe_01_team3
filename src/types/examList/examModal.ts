daf0077ed8b79a6451210e718dbd14b819306b3c
이승준 <tmdwns092727@nate.com>
2025-06-26 14:47:54 +0900

    type: 참가 모달 타입 추가
    - 200 OK 타입 추가
    - 400 Bad 타입 추가

--- src/types/examList/examModal.ts
+++ src/types/examList/examModal.ts
@@ -1,2 +1,21 @@
-/** Modal 참가 코드 타입 */
-export type ModalAccessToken = { access_code: string }
+/** POST: ModalRequest Types */
+export type ModalRequest = { access_code: string }
+
+/** 200 OK: ModalSuccessResponse Types */
+export type ExampleQuestion = {
+  question_id: number
+  type: string
+  question: string
+  options: string[]
+  point: number
+}
+
+/** 200 OK: ModalSuccessResponse Types */
+export type ModalSuccessResponse = {
+  test_id: number
+  title: string
+  thumbnail_img_url: string
+  elapsed_time: number
+  cheating_count: number
+  questions: ExampleQuestion[]
+}
