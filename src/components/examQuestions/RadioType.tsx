61ce4fb1f3acd380a1d63730c30d5b50f722880f
Lunas322 <fpvmxm333477@gmail.com>
2025-06-27 15:04:36 +0900

    Refector : style px -> 4배수 변경

--- src/components/examQuestions/RadioType.tsx
+++ src/components/examQuestions/RadioType.tsx
@@ -2,6 +2,9 @@ interface RadioTypeProps {
   options: string[]
   question_Id: number
   disabled?: boolean
+  student_answer?: string[]
+  correct_answer?: string[]
+  is_result?: boolean
 }
 
 export default function RadioType({
@@ -10,13 +13,13 @@ export default function RadioType({
   disabled = false,
 }: RadioTypeProps) {
   return (
-    <div className="w-[1000px] h-[144px] pr-[26px] pl-[32px] flex flex-col">
+    <div className="w-[1000px] h-[144px] pr-[26px] pl-8 flex flex-col">
       {options.map((option, index) => {
         return (
           <label
             className={`flex items-center cursor-pointer w-[942px] h-[27px] ${index !== 0 ? 'mt-3' : ''}`}
           >
-            <div className="relative w-[18px] h-[18px] mt-[4.5px] mb-[4.5px] mr-[12px]">
+            <div className="relative w-[18px] h-[18px] mt-[4.5px] mb-[4.5px] mr-3">
               <input
                 type="radio"
                 name={`question-${question_Id}`}
