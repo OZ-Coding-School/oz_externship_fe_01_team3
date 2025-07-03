6848984ff383fa5eea37f7654359b8aad59f0f40
han-nun <spshspshek@gmail.com>
2025-07-03 14:54:34 +0900

    Feat: MyPage와 수정 페이지 분기
    Feat: #66

---
+++ src/components/userInfo/MyPageContainer.tsx
@@ -0,0 +1,54 @@
+import SideBar from '@/components/examList/SideBar'
+import MyPage from './MyPage'
+import { useState } from 'react'
+import MyPageEdit from './MyPageEdit'
+
+export default function MyPageContainer() {
+  const [myPage, setMyPage] = useState(true)
+
+  const handleMyPageEdit = () => {
+    setMyPage(false)
+  }
+
+  return (
+    <div className="mt-[108px] min-h-screen">
+      <div className="container mx-auto min-h-screen max-w-7xl px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-24">
+        <div className="flex flex-col lg:flex-row">
+          <div className="w-full border-gray-200 lg:min-h-screen lg:w-64">
+            <div className="px-6 py-8">
+              <SideBar />
+            </div>
+          </div>
+
+          <div className="flex-1 lg:min-h-screen">
+            <div className="px-4 py-6 sm:px-6 lg:px-8">
+              <header className="h12 mb-5 flex w-186 justify-between">
+                <h1 className="text-[32px] font-bold text-[#121212]">
+                  내 정보
+                </h1>
+                {myPage ? (
+                  <button
+                    className="bg-purple-primary h-12 w-32 cursor-pointer rounded-sm text-[#ffffff] hover:bg-[#4E01B3] active:bg-[#3B0186]"
+                    onClick={handleMyPageEdit}
+                  >
+                    수정하기
+                  </button>
+                ) : (
+                  <button
+                    className="bg-purple-primary h-12 w-32 cursor-pointer rounded-sm text-[#ffffff] hover:bg-[#4E01B3] active:bg-[#3B0186]"
+                    onClick={() => {
+                      console.log('저장하기')
+                    }}
+                  >
+                    저장하기
+                  </button>
+                )}
+              </header>
+              {myPage ? <MyPage /> : <MyPageEdit />}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
