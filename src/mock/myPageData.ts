2cd6e859818dd093dd435847568feafc8ad1a1c3
han-nun <spshspshek@gmail.com>
2025-07-03 09:53:45 +0900

    Feat: 내 정보 Mock 데이터 및 Mock 데이터 타입 설정
    Feat: #66

---
+++ src/mock/myPageData.ts
@@ -0,0 +1,12 @@
+import type { MyPage } from '@/types/mypage/myPage'
+
+export const mockMyPageData: MyPage = {
+  profile_image_url: 'https://cdn.oz.com/profiles/user1.jpg',
+  email: 'user@example.com',
+  nickname: 'gildonghong',
+  name: '홍길동',
+  phone_number: '010-1234-5678',
+  birthday: '2025-07-02',
+  course_name: '123123',
+  generation: '123123',
+}
