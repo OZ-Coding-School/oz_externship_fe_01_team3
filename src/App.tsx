101be3b3c28c5f4ac80ec0aceca5a7f4b06969e2
이승준 <tmdwns092727@nate.com>
2025-07-04 12:02:37 +0900

    refactor: Header 조건부 렌더링 추가
    - 헤더 조건부 렌더링 추가
    - 헤더가 불필요한 페이지, 헤더가 필요한 페이지 구분
    - App.tsx에 불필요하게 들어가있던 Header 컴포넌트 제거
    - 각자 경로에 맞도록, 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from 'react-router'
 import './App.css'
-import { Header } from '@/components/common/Header'
 import { router } from './pages/routes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -9,10 +8,8 @@ function App() {
 
   return (
     <div>
-      template
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        <Header />
       </QueryClientProvider>
     </div>
   )
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,37 +1,46 @@
 import ExampleListContainer from '@/components/examList/ExampleContainer'
 import ProtectedRoute from '@/components/ProtectedRoute'
-import { createBrowserRouter, Navigate } from 'react-router'
+import { createBrowserRouter, Outlet } from 'react-router'
 import LandingPage from './LandingPage'
+import { Header } from '@/components/common/Header'
 
 // TODO: 임시 컴포넌트들 - 실제 컴포넌트 구현 후 교체 예정입니다. (각자 파트로 교체 해주세요 😀)
 const AuthPage = () => <div>Auth Page - 구현 예정</div>
 const RestoreAccount = () => <div>Restore Account - 구현 예정</div>
 const StudentRegistration = () => <div>Student Registration - 구현 예정</div>
-const MyPageLayout = () => <div>My Page Layout - 구현 예정</div>
-const ExamListPage = () => <div>Exam List Page - 구현 예정</div>
-const QuizParticipation = () => <div>Quiz Participation - 구현 예정</div>
-const QuizExam = () => <div>Quiz Exam - 구현 예정</div>
 const QuizResult = () => <div>Quiz Result - 구현 예정</div>
 const ExamPage = () => <div>Exam Page - 구현 예정</div>
+
+const LayoutWithHeader = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+const LayoutWithoutHeader = () => {
+  return (
+    <div>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    // element: <Layout />,
+    element: <LayoutWithoutHeader />,
     children: [
       // 메인 페이지
       {
         index: true,
         element: <LandingPage />,
-      },
-      // 로그인/회원가입 - 로그인 안한 사람만
-      {
-        path: 'auth',
-        element: (
-          <ProtectedRoute requireAuth={false}>
-            <AuthPage />
-          </ProtectedRoute>
-        ),
       },
       // 탈퇴회원 복구 - 로그인 안한 사람만
       {
@@ -46,37 +55,10 @@ export const router = createBrowserRouter([
       {
         path: 'student-registration',
         element: (
-          <ProtectedRoute requireAuth>
+          <ProtectedRoute requireAuth={false}>
             <StudentRegistration />
           </ProtectedRoute>
         ),
-      },
-      // 마이페이지 쪽지시험 목록 - 로그인 필요
-      {
-        path: 'my-quiz',
-        element: (
-          <ProtectedRoute requireAuth={false}>
-            <ExampleListContainer />
-          </ProtectedRoute>
-        ),
-        children: [
-          {
-            index: true,
-            element: <Navigate to="all" replace />,
-          },
-          {
-            path: 'all',
-            element: <ExamListPage />,
-          },
-          {
-            path: 'completed',
-            element: <ExamListPage />,
-          },
-          {
-            path: 'not-started',
-            element: <ExamListPage />,
-          },
-        ],
       },
       // 시험 응시 페이지 - 로그인 필요
       {
@@ -87,30 +69,36 @@ export const router = createBrowserRouter([
           </ProtectedRoute>
         ),
       },
-      // 쪽지시험 참여 - 로그인 필요
-      {
-        path: 'quiz/:quizId',
-        element: (
-          <ProtectedRoute requireAuth>
-            <QuizParticipation />
-          </ProtectedRoute>
-        ),
-      },
-      // 쪽지시험 응시화면 - 로그인 필요
-      {
-        path: 'quiz/:quizId/exam',
-        element: (
-          <ProtectedRoute requireAuth>
-            <QuizExam />
-          </ProtectedRoute>
-        ),
-      },
       // 쪽지시험 결과확인 - 로그인 필요
       {
         path: 'quiz/:quizId/result',
         element: (
           <ProtectedRoute requireAuth>
             <QuizResult />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <LayoutWithHeader />,
+    children: [
+      // 로그인/회원가입 - 로그인 안한 사람만
+      {
+        path: 'auth',
+        element: (
+          <ProtectedRoute requireAuth={false}>
+            <AuthPage />
+          </ProtectedRoute>
+        ),
+      },
+      // 마이페이지 쪽지시험 목록 - 로그인 필요
+      {
+        path: 'my-quiz',
+        element: (
+          <ProtectedRoute requireAuth={false}>
+            <ExampleListContainer />
           </ProtectedRoute>
         ),
       },
