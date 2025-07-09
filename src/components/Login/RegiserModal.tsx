f4c931f7af77590cff61eb3a275a09438b8b5f8b
creamymood <wbwbwlgid@naver.com>
2025-07-09 14:34:12 +0900

    feat: 수강생 등록 모달 틀 UI 구현
    - 수강생 등록에 사용될 모달 틀 구현

---
+++ src/components/Login/RegiserModal.tsx
@@ -0,0 +1,38 @@
+//모달 큰 틀
+
+type StudentRegisterModalProps = {
+  setIsModalOpen: (value: boolean) => void
+  children: React.ReactNode
+}
+export default function StudentRegisterModal({
+  setIsModalOpen,
+  children,
+}: StudentRegisterModalProps) {
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center"
+      style={{ backgroundColor: 'rgba(18, 18, 18, 0.6)' }}
+    >
+      {/* 모달 내용 박스  */}
+      <div
+        className="w-[396px] rounded-[12px] bg-white"
+        style={{
+          top: '280px',
+          bottom: '278px',
+          left: '762px',
+          right: '762px',
+        }}
+      >
+        <div className="mt-[24px] mr-[24px] mb-[24px] ml-[24px] flex justify-end">
+          <img
+            src="src/assets/closeIcon.png"
+            alt="close"
+            onClick={() => setIsModalOpen(false)}
+            className="h-[12px] w-[12px]"
+          />
+        </div>
+        {children}
+      </div>
+    </div>
+  )
+}
