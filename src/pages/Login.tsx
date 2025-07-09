9bcdbd661d0c4684116b21bbce7118de0a87f6be
creamymood <wbwbwlgid@naver.com>
2025-07-09 17:39:30 +0900

    refactor : 로그인 페이지 소셜버튼 섹션을 컴포넌트로 분리
    - 소셜버튼 섹션을 컴포넌트로 분리 및 페이지에 import

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import DeactivatedAccountInfoModal from '@/components/Login/DeactivatedModal'
 import FindIdContent from '@/components/Login/FindIdContent'
 import FindModal from '@/components/Login/FindModal'
 import FindPwContent from '@/components/Login/FindPwContent'
+import LoginButtonSection from '@/components/Login/LoginButtonSection'
 import LoginHeader from '@/components/Login/LoginHeader'
 import { useLoginForm } from '@/hooks/login/useLoginForm'
 
@@ -35,29 +36,9 @@ export default function Login() {
       {/*로그인 헤더*/}
       <LoginHeader navigate={navigate} />
 
-      {/* 카카오로 회원가입 버튼  */}
-      <SocialButton
-        bgColor="#FEE500"
-        txtColor="#391C1A"
-        iconSrc="src/assets/kakao_brown.svg"
-        iconAlt="카카오"
-        marginBottom="16px"
-        onClick={() => alert('카카오 로그인 클릭됨')}
-      >
-        카카오 간편 로그인 / 가입
-      </SocialButton>
+      {/*소셜 로그인 섹션*/}
+      <LoginButtonSection />
 
-      {/* 네이버로 회원가입 버튼  */}
-      <SocialButton
-        bgColor="#03C75A"
-        txtColor="#FFFFFF"
-        iconSrc="src/assets/naver_white.svg"
-        iconAlt="네이버"
-        marginBottom="25px"
-        onClick={() => alert('네이버 로그인 클릭됨')}
-      >
-        네이버 간편 로그인 / 가입
-      </SocialButton>
       {/* 여기에 Toast 컴포넌트를 추가합니다 */}
       {toast && <Toast toast={toast} />}
 
