8d0a1b485d0b432ea5a85007494799cade658e8b
creamymood <wbwbwlgid@naver.com>
2025-07-09 14:07:25 +0900

    feat: 레지스터 페이지를 작은 컴포넌트로 분리 (헤더)
    - 레지스터 페이지 컴포넌트 분리 부분 중 헤더 파트 생성

---
+++ src/components/register/RegisterHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function RegisterHeader() {
+  return (
+    <div className="mb-[27px] flex flex-col flex-wrap items-center gap-2">
+      <p>마법같이 빠르게 성장시켜줄</p>
+      <img
+        src="src/assets/logo_black.png"
+        alt="오즈코딩스쿨 로고"
+        className="mb-[27px] h-[24px] w-[180px]"
+      />
+      <p className="mb-[20px]">회원가입</p>
+    </div>
+  )
+}
