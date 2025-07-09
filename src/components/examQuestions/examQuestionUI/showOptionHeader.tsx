32f6100840a788ed6573c82261dc1df0d91f490f
Lunas322 <fpvmxm333477@gmail.com>
2025-07-09 13:42:08 +0900

    feat: header, showOptionHeader 부정행위 카운트 추가

--- src/components/examQuestions/examQuestionUI/showOptionHeader.tsx
+++ src/components/examQuestions/examQuestionUI/showOptionHeader.tsx
@@ -3,9 +3,13 @@ import { flexCenterStyle, roundedPillStyle } from '../style'
 
 interface ShowOptionHeaderProps {
   time: number
+  cheatingCount: number
 }
 
-export default function ShowOptionHeader({ time }: ShowOptionHeaderProps) {
+export default function ShowOptionHeader({
+  time,
+  cheatingCount,
+}: ShowOptionHeaderProps) {
   return (
     <div className="flex items-center gap-6">
       <h3
@@ -21,14 +25,24 @@ export default function ShowOptionHeader({ time }: ShowOptionHeaderProps) {
           부정행위
         </h3>
         <div className="flex gap-2">
-          {[...Array(3)].map((_, index) => (
-            <span
-              key={index}
-              className={`${flexCenterStyle} h-[26.67px] w-[20px] rounded-[2px] border-[1.5px] border-[#bdbdbd] bg-[#fafafa] text-[16px] font-bold text-[#bdbdbd]`}
-            >
-              !
-            </span>
-          ))}
+          {[...Array(3)].map((_, index) => {
+            const color = 'border-[#FFAE00] bg-[#FFAE00] text-white'
+            return (
+              //TODO: 카운트 변경 로직 리펙토링
+              <span
+                key={index}
+                className={`${flexCenterStyle} * h-[26.67px] w-[20px] rounded-[2px] border-[1.5px] text-[16px] font-bold text-[#bdbdbd] ${
+                  cheatingCount > index
+                    ? index === 2 && cheatingCount >= 3
+                      ? 'border-[#EC0037] bg-[#EC0037] text-white'
+                      : color
+                    : 'border-[#bdbdbd] bg-[#fafafa] text-[#bdbdbd]'
+                }`}
+              >
+                !
+              </span>
+            )
+          })}
         </div>
       </div>
     </div>
