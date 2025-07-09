cf0c8e2da55d062a54d728605629d6b1d1c3f223
creamymood <wbwbwlgid@naver.com>
2025-07-09 13:28:35 +0900

    feat: 비밀번호 찾기, 재설정 모달 UI 구현, 관련 토스터 수정
    - 비밀번호 찾기, 재설정 모달 내용 구현
    - 관련 아이콘 추가(토스터 아이콘, 비밀번호 아이콘)
    - 토스터 일부 수정

---
+++ src/components/Login/FindPwContent.tsx
@@ -0,0 +1,251 @@
+// 비밀번호 찾기 내용 컴포넌트
+
+import { useState } from 'react'
+import CommonButton from './CommonButton'
+import { useForm } from 'react-hook-form'
+import type { ToastProps } from '@/types/common/Toast'
+import { Toast } from '../common/Toast'
+import Timer from '../common/Timer'
+
+interface FindPwContentProps {
+  email: string
+  emailCode: string
+  password: string
+  passwordConfirm: string
+}
+export default function FindPwContent() {
+  const [toast, setToast] = useState<ToastProps | null>(null)
+  const [showTimer, setShowTimer] = useState(false)
+
+  const showToast = (options: ToastProps) => {
+    setToast(options)
+    setTimeout(() => {
+      setToast(null)
+    }, 3000)
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<FindPwContentProps>({ mode: 'onBlur' })
+  const emailValue = watch('email')
+  const emailCodeValue = watch('emailCode')
+  const passwordValue = watch('password')
+  const passwordConfirmValue = watch('passwordConfirm')
+
+  // 비밀번호와 비밀번호 확인 일치 여부
+  const isPasswordMatch = passwordValue === passwordConfirmValue
+  // 비밀번호 유효성 검사 통과 여부
+  const isPasswordValid =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+\[\]{};':"\\|,.<>\/?`~\-])[A-Za-z\d!@#$%^&*()_+\[\]{};':"\\|,.<>\/?`~\-]{6,15}$/.test(
+      passwordValue
+    )
+
+  //인증 코드 설정 버튼 disabled 조건 (여기에 나중에는 우선 값이 )
+  const isVerifiedCodeDisabled = !emailCodeValue
+
+  // 비밀번호 재설정 버튼 disabled 조건
+  const isDisabled = !(
+    isPasswordValid &&
+    isPasswordMatch &&
+    !errors.password &&
+    !errors.passwordConfirm
+  )
+
+  const [emailVerified, setEmailVerified] = useState(false) //인증코드 성공 여부 (추후 백엔드 연결시, 서버에서 "성공!"이라고 오면 이 emailVerified값이 true )
+  const handleVerifyCode = () => {
+    // 현재는 UI용 임시 코드
+    if (emailCodeValue === '000000') {
+      setEmailVerified(true)
+    } else {
+      ;<p className="text-sm text-red-500">{errors.emailCode?.message}</p>
+      // 실패 처리
+    }
+  }
+
+  //인증번호 코드 전송! 함수 (토스트 띄우기, 타이머)
+  const sendVerified = () => {
+    showToast({
+      message: '전송 완료! 이메일을 확인 해주세요.',
+      type: 'success',
+      layout: 'inline',
+      className: '',
+    })
+    setShowTimer(true)
+  }
+
+  return (
+    <div>
+      {/* 여기에 Toast 컴포넌트를 추가합니다 */}
+      {toast && <Toast toast={toast} />}
+      <div className="mt-[10px] flex flex-col items-center justify-center">
+        <img
+          src="src/assets/FindPwicon.png"
+          alt="find"
+          className="mb-[16px] h-[28px] w-[28px]"
+        />
+        <p className="mb-[16px] text-[20px] leading-none font-bold text-[#121212]">
+          {emailVerified ? '비밀번호 재설정' : '비밀번호 찾기'}
+        </p>
+        <p className="text-sm leading-none text-[#4D4D4D]">
+          {emailVerified
+            ? '신규 비밀번호를 입력해주세요.'
+            : '이메일로 비밀번호 재설정 인증코드를 보내드려요.'}
+        </p>
+      </div>
+      {emailVerified ? (
+        <div className="ml-[24px]">
+          {/* 비밀번호 */}
+          <div className="flex items-center">
+            <label
+              htmlFor="password"
+              className="mr-[16px] mb-[20px] text-[#121212]"
+            >
+              새 비밀번호<span className="text-[#EC0037]">*</span>
+            </label>
+            <p className="mb-[20px] text-[14px] font-semibold text-[#6201E0]">
+              6~15자의 영문 대소문자, 숫자, 특수문자 포함
+            </p>
+          </div>
+          <input
+            type="password"
+            {...register('password', {
+              required: '비밀번호를 입력해주세요.',
+              pattern: {
+                value:
+                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+\[\]{};':"\\|,.<>\/?`~\-])[A-Za-z\d!@#$%^&*()_+\[\]{};':"\\|,.<>\/?`~\-]{6,15}$/,
+                message: '6~15자, 영문/숫자/특수문자를 모두 포함해야 합니다.',
+              },
+            })}
+            className={`mb-[16px] h-[48px] w-[348px] rounded-[4px] border-[1px] pt-[10px] pr-[16px] pb-[10px] pl-[16px] text-[#333] placeholder-[#BDBDBD] focus:border-[#6201E0] focus:outline-none ${
+              errors.password ||
+              (passwordValue &&
+                passwordConfirmValue &&
+                passwordValue !== passwordConfirmValue)
+                ? 'border-[#EC0037]' // 형식 틀리거나 확인 칸이랑 다르면 빨간색
+                : passwordValue &&
+                    passwordConfirmValue &&
+                    isPasswordValid &&
+                    isPasswordMatch
+                  ? 'border-[#14C786]' // 둘 다 맞으면 초록색
+                  : 'border-[#BDBDBD]' // 그 외엔 기본 회색
+            } `}
+            placeholder="비밀번호를 입력해주세요"
+          />
+
+          <input
+            type="password"
+            {...register('passwordConfirm', {
+              required: '비밀번호 확인을 입력해주세요.',
+              validate: (value) =>
+                value === passwordValue || '비밀번호가 일치하지 않습니다.',
+            })}
+            className={`mb-[16px] h-[48px] w-[348px] rounded-[4px] border-[1px] pt-[10px] pr-[16px] pb-[10px] pl-[16px] text-[#333] placeholder-[#BDBDBD] focus:border-[#6201E0] focus:outline-none ${
+              errors.passwordConfirm ||
+              (passwordValue &&
+                passwordConfirmValue &&
+                passwordValue !== passwordConfirmValue)
+                ? 'border-[#EC0037]'
+                : passwordValue &&
+                    passwordConfirmValue &&
+                    isPasswordValid &&
+                    isPasswordMatch
+                  ? 'border-[#14C786]'
+                  : 'border-[#BDBDBD]'
+            } `}
+            placeholder="비밀번호를 다시 입력해주세요"
+          />
+          <CommonButton
+            onClick={handleVerifyCode}
+            text="확인"
+            disabled={isDisabled}
+          />
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit(handleVerifyCode)}>
+          <div className="ml-[24px]">
+            {/* 이메일 */}
+            <div className="mt-[40px] flex items-center">
+              <label
+                htmlFor="email"
+                className="mr-[16px] mb-[20px] text-[#121212]"
+              >
+                이메일<span className="text-[#EC0037]">*</span>
+              </label>
+            </div>
+            <div className="flex">
+              <input
+                {...register('email', {
+                  required: '이메일을 입력해주세요.',
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: '유효한 이메일 주소를 입력해주세요.',
+                  },
+                })}
+                className={`h-[48px] w-[201px] rounded-[4px] border-[1px] border-[#BDBDBD] pt-[10px] pr-[16px] pb-[10px] pl-[16px] text-[#333] placeholder-[#BDBDBD] focus:border-[#6201E0] focus:outline-none ${
+                  errors.email
+                    ? 'border-red-500' // 에러가 있으면 빨간색
+                    : watch('email') && !errors.email // 값이 있고 에러가 없으면 (성공 시) 녹색
+                      ? 'border-green-500'
+                      : 'border-[#BDBDBD]' // 그 외 기본 색상
+                }`}
+                placeholder="이메일을 입력해주세요"
+              />
+              <button
+                type="button"
+                className={`ml-[12px] h-[48px] w-[139px] rounded-[4px] border ${emailValue ? 'border-[#6201E0] bg-[#EFE6FC] text-[#6201E0]' : 'border-[#BDBDBD] bg-[#ECECEC] text-[#888]'} `}
+                onClick={sendVerified}
+                disabled={!emailValue}
+              >
+                인증코드전송
+              </button>
+            </div>
+            {errors.email && (
+              <p className="mt-1 text-sm text-red-500">
+                {errors.email.message}
+              </p>
+            )}
+
+            {/* 이메일 인증코드 */}
+            <div className="mt-[16px] flex">
+              <div className="relative w-[201px]">
+                <input
+                  {...register('emailCode', {
+                    required: '인증코드를 입력해주세요.',
+                  })}
+                  className={`h-[48px] w-[201px] rounded-[4px] border-[1px] border-[#BDBDBD] pt-[10px] pr-[16px] pb-[10px] pl-[16px] text-[#333] placeholder-[#BDBDBD] focus:border-[#6201E0] focus:outline-none`}
+                  placeholder="인증번호를 입력하세요."
+                />
+                {showTimer && (
+                  <div className="text-[#EC0037]] pointer-events-none absolute top-0 right-0 flex h-full items-center pr-[16px] font-medium">
+                    <div className="text-[#EC0037]">
+                      <Timer time={5} />
+                    </div>
+                  </div>
+                )}
+              </div>
+              <button
+                className={`ml-[12px] h-[48px] w-[139px] rounded-[4px] border ${emailCodeValue ? 'border-[#6201E0] bg-[#EFE6FC] text-[#6201E0]' : 'border-[#BDBDBD] bg-[#ECECEC] text-[#888]'} `}
+              >
+                인증번호확인
+              </button>
+            </div>
+            {errors.emailCode && (
+              <p className="mt-1 text-sm text-red-500">
+                {errors.emailCode.message}
+              </p>
+            )}
+
+            <CommonButton
+              onClick={handleVerifyCode}
+              text="비밀번호찾기"
+              disabled={isVerifiedCodeDisabled}
+            />
+          </div>
+        </form>
+      )}
+    </div>
+  )
+}
--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,5 +1,5 @@
 import type { ToastProps } from '@/types/common/Toast'
-import checkIcon from '@/assets/check.svg'
+import checkIcon from '@/assets/SuccessCheck.svg'
 import errorIcon from '@/assets/alert.svg'
 
 export const Toast = ({ toast }: { toast: ToastProps | null }) => {
@@ -12,27 +12,29 @@ export const Toast = ({ toast }: { toast: ToastProps | null }) => {
 
   if (toast.layout === 'centered') {
     return (
-      <div className="fixed top-[422px] left-[762px] flex h-[128px] w-[396px] flex-col items-center justify-center gap-2 rounded-[12px] bg-[#ffffff] text-center">
-        <div className="h-[24px] w-[24px]">
-          <img src={icon} alt={toast.type} className="h-full w-full" />
-        </div>
-        <div
-          className={`text-[20px] ${centeredTitleColor} leading-[1.4] font-bold tracking-[-0.03em]`}
-        >
-          {toast.message}
-        </div>
-        {toast.subMessage && (
-          <div className="text-sm leading-[1.4] tracking-[-0.03em] text-[#4d4d4d]">
-            {toast.subMessage}
+      <div className="fixed inset-0 bg-black/60">
+        <div className="fixed top-1/2 left-1/2 flex h-[128px] w-[396px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 rounded-[12px] bg-[#ffffff] text-center">
+          <div className="h-[24px] w-[24px]">
+            <img src={icon} alt={toast.type} className="h-full w-full" />
           </div>
-        )}
+          <div
+            className={`text-[20px] ${centeredTitleColor} leading-[1.4] font-bold tracking-[-0.03em]`}
+          >
+            {toast.message}
+          </div>
+          {toast.subMessage && (
+            <div className="text-sm leading-[1.4] tracking-[-0.03em] text-[#4d4d4d]">
+              {toast.subMessage}
+            </div>
+          )}
+        </div>
       </div>
     )
   }
 
   return (
     <div
-      className={`fixed top-[204px] left-[836px] flex h-[48px] w-[248px] items-center rounded-md border border-[#ECECEC] bg-[#fafafa] px-3 pl-2 [box-shadow:4px_4px_4px_0_rgba(131,131,131,0.25)] ${toast.className || ''}`}
+      className={`fixed top-[100px] left-1/2 flex h-[48px] w-[240px] -translate-x-1/2 items-center rounded-md border border-[#ECECEC] bg-[#fafafa] px-3 pl-2 [box-shadow:4px_4px_4px_0_rgba(131,131,131,0.25)] ${toast.className || ''}`}
     >
       <img src={icon} alt={toast.type} className="mr-1 h-[24px] w-[24px]" />
       <span
