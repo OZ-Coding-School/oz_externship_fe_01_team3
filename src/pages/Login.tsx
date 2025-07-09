f193083df8ca99cd4ef9ca47f525eaa2c4cf7787
creamymood <wbwbwlgid@naver.com>
2025-07-09 13:15:39 +0900

    feat : 로그인 메인 페이지 UI 구현 및 리액트 훅폼 설치, 라우터 설정
    - 리액트 훅폼 설치
    - 로그인 메인페이지 UI 구현
    - 로그인 페이지에 필요한 라우터 설정

---
+++ src/pages/Login.tsx
@@ -0,0 +1,224 @@
+import SocialButton from '@/components/common/SocialButton'
+import { Toast } from '@/components/common/Toast'
+import DeactivatedAccountInfoModal from '@/components/Login/DeactivatedModal'
+import FindIdContent from '@/components/Login/FindIdContent'
+import FindModal from '@/components/Login/FindModal'
+import FindPwContent from '@/components/Login/FindPwContent'
+import type { ToastProps } from '@/types/common/Toast'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router'
+// 지향 파트 (짧아지는게 목적이긴한데, 그렇다고 극단적으로 다 지울필요는 없어요.)
+// TODO: 컴포넌트를 세분화해서 쪼개는게 일단 우선이긴한데,,
+// 생각보다 쪼개야하는 컴포넌트의 수가 많아서, 로그인, 회원가입 언제까지 가능할지...
+
+export default function Login() {
+  const navigate = useNavigate()
+  const [toast, setToast] = useState<ToastProps | null>(null)
+
+  const showToast = (options: ToastProps) => {
+    setToast(options)
+    setTimeout(() => {
+      setToast(null)
+    }, 3000)
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+  } = useForm({ mode: 'onChange' })
+
+  const onSubmit = (data: any) => {
+    const { email, password } = data
+    //임시 로직 (가짜 아이디 비밀번호 성공시, ui구현)
+    if (email === 'test@test.com' && password === 'QWERTY12!') {
+      showToast({
+        message: '로그인 성공!',
+        type: 'success',
+        layout: 'centered',
+        subMessage: '메인 페이지로 이동합니다.',
+        className: '',
+      })
+      navigate('/') //랜딩페이지로 이동 (근데 로그인 상태 유지는? 모르겠음)
+    } //탈퇴회원일 경우 로직
+    else if (email === 'test1@test.com' && password === 'QWERTY12!!') {
+      setIsModalOpen(true)
+      setModalContentType('deactivatedInfo')
+    } else {
+      // 로그인 정보 없어서 실패 시
+      showToast({
+        message: '로그인 실패!',
+        type: 'error',
+        layout: 'centered',
+        subMessage: '이메일 또는 비밀번호를 확인해주세요.',
+      })
+      reset() //인풋창 리셋 시켜버림
+    }
+  }
+  //이후에 로그인 성공 시, 메인 페이지로 이동 / 로그인 실패시, 오류시 오류 구현 필요
+  //onSubmit 함수에, 탈퇴회원 로직도 추가 필요
+
+  const emailValue = watch('email')
+  const passwordValue = watch('password')
+
+  const isAllFieldsFilled = !!emailValue && !!passwordValue
+
+  //모달 관련 상태
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const [modalContentType, setModalContentType] = useState<
+    'findIdType' | 'findPwType' | 'deactivatedInfo'
+  >('findIdType') //모달에 어떤  찾기 화면 보여줄지 상태(아이디찾기, 비밀번호 찾기)
+
+  return (
+    <div className="flex flex-col items-center pt-20">
+      <img
+        src="src/assets/logo_black.png"
+        alt="오즈코딩스쿨 로고"
+        className="mb-[27px] w-[180px]"
+      />
+      <div className="flex gap-3 text-[16px]">
+        <p className="text-[#4D4D4D]"> 아직 회원이 아니신가요?</p>
+        <p
+          className="cursor-pointer text-[#6201E0]"
+          onClick={() => navigate('/join')}
+        >
+          회원가입 하기
+        </p>
+      </div>
+      <br />
+      <br />
+
+      {/* 카카오로 회원가입 버튼  */}
+      <SocialButton
+        bgColor="#FEE500"
+        txtColor="#391C1A"
+        iconSrc="src/assets/kakao_brown.svg"
+        iconAlt="카카오"
+        marginBottom="16px"
+        onClick={() => alert('카카오 로그인 클릭됨')}
+      >
+        카카오 간편 로그인 / 가입
+      </SocialButton>
+
+      {/* 네이버로 회원가입 버튼  */}
+      <SocialButton
+        bgColor="#03C75A"
+        txtColor="#FFFFFF"
+        iconSrc="src/assets/naver_white.svg"
+        iconAlt="네이버"
+        marginBottom="25px"
+        onClick={() => alert('네이버 로그인 클릭됨')}
+      >
+        네이버 간편 로그인 / 가입
+      </SocialButton>
+      {/* 여기에 Toast 컴포넌트를 추가합니다 */}
+      {toast && <Toast toast={toast} />}
+
+      {/*아이디*/}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center"
+      >
+        {/* 이메일 인풋 */}
+        <div className="mb-[16px]">
+          <input
+            {...register('email', {
+              required: '이메일을 입력해주세요.',
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: '유효한 이메일 주소를 입력해주세요.',
+              },
+            })}
+            className={`autofill h-[48px] w-[348px] rounded-[4px] border-[1px] px-[16px] py-[10px] text-sm text-[#333] placeholder-[#BDBDBD] focus:border-[#6201E0] focus:outline-none ${
+              errors.email
+                ? 'border-red-500' // 에러가 있으면 빨간색
+                : watch('email') && !errors.email // 값이 있고 에러가 없으면 (성공 시) 녹색
+                  ? 'border-green-500'
+                  : 'border-[#BDBDBD]' // 그 외 기본 색상
+            }`}
+            placeholder="아이디 (example@gmail.com)"
+          />
+        </div>
+
+        {/* 비밀번호 인풋 */}
+        <div className="mb-[16px]">
+          <input
+            type="password"
+            {...register('password', {
+              required: '비밀번호를 입력해주세요.',
+              pattern: {
+                value:
+                  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()[\]{};:'",.<>/?\\|~`_\-=+]).{6,15}$/,
+                message: '6~15자, 영문/숫자/특수문자를 모두 포함해야 합니다.',
+              },
+            })}
+            className={`autofill h-[48px] w-[348px] rounded-[4px] border-[1px] px-[16px] py-[10px] text-sm text-[#333] placeholder-[#BDBDBD] focus:border-[#6201E0] focus:outline-none ${
+              errors.password
+                ? 'border-red-500' // 에러가 있으면 빨간색
+                : watch('password') && !errors.password // 값이 있고 에러가 없으면 (성공 시) 녹색
+                  ? 'border-green-500'
+                  : 'border-[#BDBDBD]' // 그 외 기본 색상
+            }`}
+            placeholder="비밀번호 (6~15자의 영문 대소문자, 숫자, 특수문자)"
+          />
+        </div>
+
+        <div className="flex w-full justify-start text-sm text-[#4D4D4D]">
+          <button
+            type="button"
+            onClick={() => {
+              setIsModalOpen(true)
+              setModalContentType('findIdType')
+            }}
+            className="mr-[8px]"
+          >
+            아이디 찾기
+          </button>
+
+          {/*모달이 열릴 때, 모달컨텐트타입 상태가, findIdType이면 아이디찾기 내용 보여주기 / findPwContent면 비밀번호 찾기 내용 보여주기 렌더링 관련 */}
+
+          {isModalOpen && (
+            <FindModal
+              onClose={() => {
+                setIsModalOpen(false)
+              }}
+            >
+              {modalContentType === 'findIdType' && (
+                <FindIdContent setModalContentType={setModalContentType} />
+              )}
+              {modalContentType === 'findPwType' && <FindPwContent />}
+              {modalContentType === 'deactivatedInfo' && (
+                <DeactivatedAccountInfoModal />
+              )}
+            </FindModal>
+          )}
+
+          <p className="mr-[8px]">|</p>
+
+          <button
+            type="button"
+            onClick={() => {
+              setIsModalOpen(true)
+              setModalContentType('findPwType')
+            }}
+          >
+            비밀번호 찾기
+          </button>
+        </div>
+
+        {/* 일반회원 로그인 버튼 : 유효성 검사 미통과시 disabled / 유효성 다 통과하고 값이 있어야만 보라색 활성화 색 */}
+        <button
+          type="submit"
+          disabled={!!errors.email || !!errors.password}
+          className={`mt-[12px] h-[52px] w-[348px] gap-2 rounded-[4px] ${!errors.email && !errors.password && isAllFieldsFilled ? 'bg-[#6201E0] text-white' : 'bg-[#ECECEC] text-[#BDBDBD]'} `}
+        >
+          일반회원 로그인
+        </button>
+      </form>
+    </div>
+  )
+}
