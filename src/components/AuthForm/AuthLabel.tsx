709e288c8d58f114fe064a1f4e66a84c971df883
creamymood <wbwbwlgid@naver.com>
2025-07-09 22:59:22 +0900

    refactor : AuthLabel 파일 일부 수정

--- src/components/AuthForm/AuthLabel.tsx
+++ src/components/AuthForm/AuthLabel.tsx
@@ -1,11 +1,19 @@
 interface AuthLabelProps {
   htmlFor: string
   children: React.ReactNode
+  className?: string
 }
 
-export default function AuthLabel({ htmlFor, children }: AuthLabelProps) {
+export default function AuthLabel({
+  htmlFor,
+  children,
+  className,
+}: AuthLabelProps) {
   return (
-    <label htmlFor={htmlFor} className="mr-[16px] mb-[20px] text-[#121212]">
+    <label
+      htmlFor={htmlFor}
+      className={`mr-[16px] mb-[20px] text-[#121212] ${className ?? ''}`}
+    >
       {children}
       <span className="text-[#EC0037]">*</span>
     </label>
