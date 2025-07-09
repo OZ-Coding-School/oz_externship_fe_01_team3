4fc633af16f222777e4fd5b90fc53cc1e25bff78
creamymood <wbwbwlgid@naver.com>
2025-07-09 14:11:32 +0900

    feat: 레지스터 내 인증번호 버튼 공통 컴포넌트
    - 레지스터 페이지에서 자주 쓰이는 인증 번호 버튼 공통 컴포넌트 생성

---
+++ src/components/AuthForm/ValidateButton.tsx
@@ -0,0 +1,28 @@
+import type { HtmlHTMLAttributes } from 'react'
+import type React from 'react'
+
+interface ValidateButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'active' | 'inactive' // variant prop 추가 (필요에 따라 조절)
+}
+
+export default function ValidateButton({
+  variant,
+  className,
+  children,
+  ...rest
+}: ValidateButtonProps) {
+  const baseStyle = 'ml-[12px] h-[48px] w-[112px] rounded-[4px] border'
+  const variantStyle =
+    variant === 'active'
+      ? 'border-[#6201E0] bg-[#EFE6FC] text-[#6201E0]'
+      : 'border-[#BDBDBD] bg-[#ECECEC] text-[#888]'
+  return (
+    <button
+      className={`${baseStyle} ${variantStyle} ${className ?? ''} `}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
+}
