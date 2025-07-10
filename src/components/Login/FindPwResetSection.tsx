2200bc9be0d1f97aa2456edf7248b10be31a0076
creamymood <wbwbwlgid@naver.com>
2025-07-10 10:55:53 +0900

    refactor : 비밀번호 재설정 모달 부분 리팩토링 (공통인풋,라벨로 변경)

--- src/components/Login/FindPwResetSection.tsx
+++ src/components/Login/FindPwResetSection.tsx
@@ -1,9 +1,13 @@
+// 비밀번호 찾기 모달 중 조건부 렌더링 중 비밀번호 리셋 섹션
+
 import type { RegisterFormData } from '@/types/login/register'
 import type {
   FieldErrors,
   UseFormRegister,
   UseFormWatch,
 } from 'react-hook-form'
+import AuthLabel from '../AuthForm/AuthLabel'
+import AuthInput from '../AuthForm/AuthInput'
 
 interface FindPwResetSectionProps {
   register: UseFormRegister<RegisterFormData>
@@ -27,7 +31,11 @@ export default function FindPwResetSection({
     <>
       {/* 비밀번호 */}
       <div className="mt-[40px] flex items-center">
-        <label
+        <AuthLabel htmlFor="password">새 비밀번호</AuthLabel>
+        <p className="text-[14px] font-semibold text-[#6201E0]">
+          6~15자의 영문 대소문자, 숫자, 특수문자 포함
+        </p>
+        {/* <label
           htmlFor="password"
           className="mr-[16px] mb-[20px] text-[#121212]"
         >
@@ -35,9 +43,9 @@ export default function FindPwResetSection({
         </label>
         <p className="mb-[20px] text-[14px] font-semibold text-[#6201E0]">
           6~15자의 영문 대소문자, 숫자, 특수문자 포함
-        </p>
+        </p> */}
       </div>
-      <input
+      <AuthInput
         type="password"
         {...register('password', {
           required: '비밀번호를 입력해주세요.',
@@ -47,43 +55,38 @@ export default function FindPwResetSection({
             message: '6~15자, 영문/숫자/특수문자를 모두 포함해야 합니다.',
           },
         })}
-        className={`mb-[16px] h-[48px] w-[348px] rounded-[4px] border-[1px] pt-[10px] pr-[16px] pb-[10px] pl-[16px] text-[#333] placeholder-[#BDBDBD] focus:border-[#6201E0] focus:outline-none ${
-          errors.password ||
-          (passwordValue &&
-            passwordConfirmValue &&
-            passwordValue !== passwordConfirmValue)
-            ? 'border-[#EC0037]' // 형식 틀리거나 확인 칸이랑 다르면 빨간색
-            : passwordValue &&
-                passwordConfirmValue &&
-                isPasswordValid &&
-                isPasswordMatch
-              ? 'border-[#14C786]' // 둘 다 맞으면 초록색
-              : 'border-[#BDBDBD]' // 그 외엔 기본 회색
-        } `}
         placeholder="비밀번호를 입력해주세요"
+        variant={
+          passwordValue &&
+          passwordConfirmValue &&
+          passwordValue !== passwordConfirmValue
+            ? 'error'
+            : passwordValue && passwordConfirmValue && isPasswordMatch
+              ? 'success'
+              : 'default'
+        }
+        className="w-[348px]"
       />
 
-      <input
+      <AuthInput
         type="password"
         {...register('passwordConfirm', {
           required: '비밀번호 확인을 입력해주세요.',
           validate: (value) =>
             value === passwordValue || '비밀번호가 일치하지 않습니다.',
         })}
-        className={`mb-[16px] h-[48px] w-[348px] rounded-[4px] border-[1px] pt-[10px] pr-[16px] pb-[10px] pl-[16px] text-[#333] placeholder-[#BDBDBD] focus:border-[#6201E0] focus:outline-none ${
+        placeholder="비밀번호를 다시 입력해주세요"
+        variant={
           errors.passwordConfirm ||
           (passwordValue &&
             passwordConfirmValue &&
             passwordValue !== passwordConfirmValue)
-            ? 'border-[#EC0037]'
-            : passwordValue &&
-                passwordConfirmValue &&
-                isPasswordValid &&
-                isPasswordMatch
-              ? 'border-[#14C786]'
-              : 'border-[#BDBDBD]'
-        } `}
-        placeholder="비밀번호를 다시 입력해주세요"
+            ? 'error'
+            : passwordValue && passwordConfirmValue && isPasswordMatch
+              ? 'success'
+              : 'default'
+        }
+        className="mt-[16px] w-[348px]"
       />
     </>
   )
