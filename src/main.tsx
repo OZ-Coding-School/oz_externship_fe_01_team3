2661641aef7a188501cf4440b7116906fa05bd59
이승준 <tmdwns092727@nate.com>
2025-07-10 19:02:15 +0900

    feat: MSW API Mocking Settings

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,24 @@ import './index.css'
 import App from '@/App.tsx'
 import { ToastBoxProvider } from './context/ToastBoxProvider'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ToastBoxProvider>
-      <App />
-    </ToastBoxProvider>
-  </StrictMode>
-)
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+
+  const { worker } = await import('./mock/browser')
+
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <ToastBoxProvider>
+        <App />
+      </ToastBoxProvider>
+    </StrictMode>
+  )
+})
