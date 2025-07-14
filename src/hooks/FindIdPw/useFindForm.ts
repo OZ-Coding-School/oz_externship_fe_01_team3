3db31a93606c51ed0663aed8be0b4d4cfc81e444
creamymood <wbwbwlgid@naver.com>
2025-07-14 21:45:44 +0900

    feat: 아이디 찾기 api 연결 후, 찾아진 아이디로 렌더링
    - 아이디 찾기 버튼에 api 연굴
    - 아이디 마스킹 되어 렌더링

--- src/hooks/FindIdPw/useFindForm.ts
+++ src/hooks/FindIdPw/useFindForm.ts
@@ -1,7 +1,6 @@
 //아이디 / 비밀번호 찾기에서 사용되는 공통 함수, 상태, 유효성 검사
 
 import type { RegisterFormData } from '@/types/login/register'
-import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 export const useFindForm = (
@@ -41,26 +40,26 @@ export const useFindForm = (
     setModalContentType('findPwType')
   }
 
-  const [errorMessage, setErrorMessage] = useState('')
-  const [successFindId, setSuccessFindId] = useState(false) //성공시 모달 보여줄 용
-  const [foundId, setFoundId] = useState('') //백엔드 연결 전 가짜 아이디 저장용
+  // const [errorMessage, setErrorMessage] = useState('')
+  // const [successFindId, setSuccessFindId] = useState(false) //성공시 모달 보여줄 용
+  // const [foundId, setFoundId] = useState('') //백엔드 연결 전 가짜 아이디 저장용
 
-  //백엔드 전 임시 (오류메세지 설정용)
-  const findId = (data: any) => {
-    console.log('제출된 데이터:', data)
+  // //백엔드 전 임시 (오류메세지 설정용)
+  // const findId = (data: any) => {
+  //   console.log('제출된 데이터:', data)
 
-    if (data.name === '홍길동') {
-      setErrorMessage('')
-      setSuccessFindId(true)
-      setFoundId('hong123@ozcoding.com')
-      // 성공 시 처리
-    } else {
-      setErrorMessage(
-        '입력한 이름과 휴대폰 번호로 등록된\n 이메일이 존재하지 않습니다. '
-      )
-      setSuccessFindId(false)
-    }
-  }
+  //   if (data.name === '홍길동') {
+  //     setErrorMessage('')
+  //     setSuccessFindId(true)
+  //     setFoundId('hong123@ozcoding.com')
+  //     // 성공 시 처리
+  //   } else {
+  //     setErrorMessage(
+  //       '입력한 이름과 휴대폰 번호로 등록된\n 이메일이 존재하지 않습니다. '
+  //     )
+  //     setSuccessFindId(false)
+  //   }
+  // }
   return {
     maskEmailDomain,
     register,
@@ -68,9 +67,10 @@ export const useFindForm = (
     errors,
     ...inputValue,
     handleClickFindPw,
-    errorMessage,
-    successFindId,
-    foundId,
-    findId,
+    // errorMessage,
+    // successFindId,
+    // setSuccessFindId,
+    // foundId,
+    // findId,
   }
 }
